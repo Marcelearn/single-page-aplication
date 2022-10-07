0ca334d0198ee9c6003490309e371e726af2ee55
Marcelearn <jaworskimarcel@gmail.com>
2022-10-07 20:08:07 +0200

    Try throwError and Catch

--- rapid-api/index.js
+++ rapid-api/index.js
@@ -1,28 +1,34 @@
-let randomArtist = ['queen','daftpunk','thedoors', 'jimihendrix']
+let randomArtist = ["ninorota", "enniomorricone"];
 async function getApi(query){
     console.log(query);
     query.forEach(async (e) => {
-        const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${e}`,{
+        try {
+            const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${e}`,{
             "method":"GET", 
-                "headers": {
+            "headers": {
                 'X-RapidAPI-Key': '444873c061msh059d5294c75a105p1a1a72jsn914e2350ba3d',
                 'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+            }});
+            if(response.status === 200) {
+                const songs = await response.json()
+                console.log (songs);
+                fetchAlbums(songs.data);
+               
+            }else {
+                throw new Error(`Could not found Artist: ${response.status}`);
             }
-        });
-        const songs = await response.json()
-        console.log (songs);
-        fetchAlbums(songs.data);    
+        }catch (error){
+            return console.log(error);
+        }  
     });
-
 }
 async function fetchAlbums(songs){
     songs.forEach(song => {
         let albumCard = document.querySelector('.container');
         albumCard.innerHTML += `<div>
         <img src="${song.album.cover_medium}"/> 
-        <p>${song.title}</p>
+        <p>${song.title_short}</p>
         </div>`
-
     });
 } 
 getApi(randomArtist)
